82499eb8311c8a90f5cabf50aa251870befdddf1
Youssef Mohamed <48146406+youssefMoJo@users.noreply.github.com>
2025-04-21 10:40:29 -0230

    add BecomeVendor page and route, apply Footer across entire app

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import NavBar from "../src/components/NavBar";
+import BecomeVendor from "./pages/BecomeVendor";
+import ROUTES from "./routes";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -9,8 +12,10 @@ function App() {
       <div className="App">
         <NavBar />
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path={ROUTES.HOME} element={<Home />} />
+          <Route path={ROUTES.BECOME_A_VENDOR} element={<BecomeVendor />} />
         </Routes>
+        <Footer />
       </div>
     </Router>
   );
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -13,7 +13,6 @@ export default function Home() {
       <UpcommingEvents />
       <Reviews />
       <Welcome />
-      <Footer />
     </div>
   );
 }
